06b47362f0a0ee5f1e4a7bdb97de6057d351b86a
Mario Köstl <mkoestl8753@cosmoconsult.com>
2018-08-20 15:10:44 +0200

    the other stuff

---
+++ src/dataStore/mockData.js
@@ -0,0 +1,7 @@
+export var UserData = [{
+    Name: "John Doe",
+    Phone1: "34253456",
+    Phone2: "987",
+    EMail1: "sdf@sdf",
+    EMail2: "df@sdf"
+}];
---
+++ src/services/mockService.js
@@ -0,0 +1,5 @@
+import * as data from "../dataStore/mockData";
+
+export function getUserData(){
+    return data.UserData;
+}
